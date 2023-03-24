857dae00b2ab52dbd2a5f7a6779e792feb806d3a
Sofhila Wahiddin <55935938+wahidOne@users.noreply.github.com>
2023-03-24 09:44:13 +0700

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,8 +1,41 @@
-yum install httpd wget unzip -y
-systemctl start httpd
-systemctl enable httpd
-cd /tmp/
+#!/bin/bash
+
+# Install Dependencies
+echo "#######################"
+echo " Installing Packages..."
+echo "#######################"
+sudo yum install httpd wget unzip -y
+echo
+
+# Start & Enable System
+echo "#######################"
+echo " Start System.........."
+echo "#######################"
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Creating Tmp Directory
+echo "#######################"
+echo " Creating Directory...."
+echo "#######################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
 wget https://www.tooplate.com/zip-templates/2119_gymso_fitness.zip
 unzip -o 2119_gymso_fitness.zip
-cp -r 2119_gymso_fitness/* /var/www/html/
+sudo cp -r 2119_gymso_fitness/* /var/www/html/
+
+# Bounce Service
+echo "############################"
+echo " Restarting HTTPD Service..."
+echo "############################"
 systemctl restart httpd
+echo
+
+# Clean Up
+echo "############################"
+echo " Removing Temp Directory...."
+echo "############################"
+rm -rf /tmp/webfiles
+echo
